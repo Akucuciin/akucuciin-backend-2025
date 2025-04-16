ece488f44f74feeb1487c21fccc098c7a5745271
unknown <anargya115@gmail.com>
2025-04-16 22:31:16 +0700

    feat:add getMitraProfile query

--- src/database/queries/mitra.query.mjs
+++ src/database/queries/mitra.query.mjs
@@ -6,7 +6,16 @@ const MitraQuery = {
             email,
         ]);
         return results[0];
-    }
+    },
+    getMitraProfile: async function (laundry_partner_id) {
+        const [results] = await db.query(
+            `SELECT id, name, email, description, telephone, address, maps_pinpoint, city, area, latitude, longitude, created_at, updated_at FROM laundry_partners WHERE id = ?`, [
+                laundry_partner_id
+            ]
+        );
+        return results[0];
+    },
+    
 }
 
 export default MitraQuery;
