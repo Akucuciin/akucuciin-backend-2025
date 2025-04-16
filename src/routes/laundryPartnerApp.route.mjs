95096543abdfa16059a6c2d3e9206520877f5a9d
unknown <anargya115@gmail.com>
2025-04-17 00:10:41 +0700

    feat: add getProfile route

---
+++ src/routes/laundryPartnerApp.route.mjs
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import LaundryPartnerAppController from "../controllers/laundryPartnerApp.mjs";
+import { authorize } from "passport";
+
+const LaundryPartnerAppRouter = Router();
+
+LaundryPartnerAppRouter.get(
+    "/api/laundry_partner/app/profile",
+    authorize("laundry-partner-jwt"),
+    async (req, res, next) => {
+        LaundryPartnerAppController.getProfile(req ,res, next);
+    }
+)
+
+export default LaundryPartnerAppRouter;
