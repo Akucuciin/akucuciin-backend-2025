46834d87c9cc7b5f969fe7342d17cf7ccbd0d70c
unknown <anargya115@gmail.com>
2025-04-17 00:13:07 +0700

    fix: add authorize jwt in updateProfile route

--- src/routes/laundryPartnerApp.route.mjs
+++ src/routes/laundryPartnerApp.route.mjs
@@ -14,6 +14,7 @@ LaundryPartnerAppRouter.get(
 
 LaundryPartnerAppRouter.put(
     "/api/laundry_partner/app/profile",
+    authorize("laundry-partner-jwt"),
     async (req, res, next) => {
         LaundryPartnerAppController.updateProfile(req ,res, next);
     }
