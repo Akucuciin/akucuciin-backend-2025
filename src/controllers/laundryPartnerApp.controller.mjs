afb259ca9f312a7ece4e4c881e5ce5d5a3baacfc
unknown <anargya115@gmail.com>
2025-05-09 20:55:24 +0700

    feat: make updatePriceOrder controller

--- src/controllers/laundryPartnerApp.controller.mjs
+++ src/controllers/laundryPartnerApp.controller.mjs
@@ -55,6 +55,17 @@ const LaundryPartnerAppController = {
         } catch (error) {
             next(error);
         }
+    },
+    updatePriceOrder: async (req, res, next) => {
+        try {
+            const result = await LaundryPartnerAppService.updatePriceOrder(req);
+            return res.status(200).json({
+                success: true,
+                data: result,
+            })
+        } catch (error) {
+            next(error);
+        }
     }
 }
 
