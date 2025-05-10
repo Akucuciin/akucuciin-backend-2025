bfd5ce1bc8f6231867721272c979a2be8c28bec7
AghnatHs <aghnathasya@gmail.com>
2025-05-10 21:41:14 +0700

    Fix: Partner can update order even though status is already 'selesai'

--- src/services/laundryPartnerApp.service.mjs
+++ src/services/laundryPartnerApp.service.mjs
@@ -1,34 +1,38 @@
 import LaundryPartnerAppQuery from "../database/queries/laundryPartnerApp.query.mjs";
-import LaundryPartnerAppSchema from "../validators/laundryPartnerApp.schema.mjs";
-import validate from "../validators/validator.mjs";
 import { BadRequestError } from "../errors/customErrors.mjs";
 import formatOrdersFromDb from "../utils/order.utils.mjs";
+import LaundryPartnerAppSchema from "../validators/laundryPartnerApp.schema.mjs";
+import validate from "../validators/validator.mjs";
 
 const LaundryPartnerAppService = {
   //Profile Create
   getProfile: async (req) => {
     const email = req.user.email;
-    const profileLaundryPartner = await LaundryPartnerAppQuery.getProfile(email);
+    const profileLaundryPartner = await LaundryPartnerAppQuery.getProfile(
+      email
+    );
     return profileLaundryPartner;
   },
   //Get and Edit Order
   getOrderById: async (req) => {
-    const {id: order_id} = req.params;
+    const { id: order_id } = req.params;
     const orderById = await LaundryPartnerAppQuery.getOrderById(order_id);
 
     if (orderById[0].lp_id !== req.user.id) {
       throw new BadRequestError("Access denied. This order is not yours.");
     }
-  
+
     const orderByIdFormated = formatOrdersFromDb(orderById);
     return orderByIdFormated;
   },
   getOrdersByLaundryPartnerId: async (req) => {
     const laundry_partner_id = req.user.id;
-    const orders = await LaundryPartnerAppQuery.getOrdersByLaundryPartnerId(laundry_partner_id);
+    const orders = await LaundryPartnerAppQuery.getOrdersByLaundryPartnerId(
+      laundry_partner_id
+    );
 
     const ordersFormatted = formatOrdersFromDb(orders);
-    return ordersFormatted
+    return ordersFormatted;
   },
   updateStatusOrder: async (req) => {
     const { id: order_id } = req.params;
@@ -40,7 +44,7 @@ const LaundryPartnerAppService = {
       throw new BadRequestError("Access denied. This order is not yours.");
     }
 
-    if (order.status === "batal")
+    if (order.status === "batal" || order.status === "selesai")
       throw new BadRequestError(
         `Failed, order status is already [${order.status}]`
       );
@@ -54,7 +58,7 @@ const LaundryPartnerAppService = {
     const result = await LaundryPartnerAppQuery.updateStatusOrder(
       values.order_id,
       values.status,
-      values.weight,
+      values.weight
     );
 
     if (!result.affectedRows) throw new BadRequestError("Failed to update");
@@ -71,6 +75,11 @@ const LaundryPartnerAppService = {
       throw new BadRequestError("Access denied. This order is not yours.");
     }
 
+    if (order.status === "batal" || order.status === "selesai")
+      throw new BadRequestError(
+        `Failed, order status is already [${order.status}]`
+      );
+
     const values = {
       order_id,
       price: updated.price || order.price,
@@ -78,13 +87,13 @@ const LaundryPartnerAppService = {
 
     const result = await LaundryPartnerAppQuery.updatePriceOrder(
       values.order_id,
-      values.price,
+      values.price
     );
 
     if (!result.affectedRows) throw new BadRequestError("Failed to update");
 
     return values;
-  }
+  },
 };
 
 export default LaundryPartnerAppService;
