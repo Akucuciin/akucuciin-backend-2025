4cc74d77dc0f66b1d265a7a901fa853dadea7863
AghnatHs <aghnathasya@gmail.com>
2025-05-13 23:52:07 +0700

    Feat: change invoice_number format to name:order_id

--- src/routes/webhook.route.mjs
+++ src/routes/webhook.route.mjs
@@ -4,14 +4,15 @@ const WebhookRouter = Router();
 
 WebhookRouter.post("/api/payment/webhook", async (req, res) => {
   const notification = req.body;
-  const orderId = notification.order.invoice_number;
+  const rawInvoice = notification.order.invoice_number;
+  const [name, orderId] = rawInvoice.split("::"); // name::orderId
   const status = notification.transaction.status;
 
   if (status === "SUCCESS") {
     await OrderQuery.updateStatusPayment(orderId, "sudah bayar");
     console.log("Pembayaran sukses untuk order", orderId);
   }
-  
+
   res.status(200).send("OK");
 });
 
