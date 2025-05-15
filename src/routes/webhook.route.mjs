55fe1b0ca0ad44cf72b32fea1306f4d4050f510e
AghnatHs <aghnathasya@gmail.com>
2025-05-15 22:38:04 +0700

    Feat: notify Customer when payment is succesfull

--- src/routes/webhook.route.mjs
+++ src/routes/webhook.route.mjs
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import OrderQuery from "../database/queries/order.query.mjs";
+import { sendOrderPaymentCompletedToCustomer } from "../services/whatsapp.service.mjs";
+import { formatOrderFromDb } from "../utils/order.utils.mjs";
 
 const WebhookRouter = Router();
 
 WebhookRouter.post("/api/payment/webhook", async (req, res) => {
   const notification = req.body;
   const rawInvoice = notification.order.invoice_number;
-  const [name, orderId, unique] = rawInvoice.split("::"); // name::orderId
+  const [name, orderId, unique] = rawInvoice.split("::"); // name::orderId::unique
   const status = notification.transaction.status;
+
+  const ordersJoined = await OrderQuery.getOrderJoinedById(orderId);
+  const orderJoined = ordersJoined[0];
+  const _order = formatOrderFromDb(orderJoined);
+
+  await sendOrderPaymentCompletedToCustomer(_order);
 
   if (status === "SUCCESS") {
     await OrderQuery.updateStatusPayment(orderId, "sudah bayar");
