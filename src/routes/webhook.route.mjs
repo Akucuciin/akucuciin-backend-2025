d39d3de2f34c4f30954419ce24c7afe3baab8112
AghnatHs <aghnathasya@gmail.com>
2025-05-19 13:02:47 +0700

    Feat: Google Auth for Customer

--- src/routes/webhook.route.mjs
+++ src/routes/webhook.route.mjs
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import passport from "../auth/passport.auth.mjs";
+import AppConfig from "../configs/app.config.mjs";
+import AuthQuery from "../database/queries/auth.query.mjs";
 import OrderQuery from "../database/queries/order.query.mjs";
+import TokenService from "../services/token.service.mjs";
 import { sendOrderPaymentCompletedToCustomer } from "../services/whatsapp.service.mjs";
 import { formatOrderFromDb } from "../utils/order.utils.mjs";
 
@@ -23,5 +27,32 @@ WebhookRouter.post("/api/payment/webhook", async (req, res) => {
 
   res.status(200).send("OK");
 });
+
+WebhookRouter.get(
+  "/api/auth/customer/google/webhook",
+  passport.authenticate("customer-google-auth", {
+    session: false,
+    failureRedirect: `${AppConfig.Server.frontendBaseUrl}/login`,
+  }),
+  async (req, res) => {
+    const { id, email } = req.user;
+    const accessToken = TokenService.generateAccessToken(id, email);
+    const refreshToken = TokenService.generateRefreshToken(id, email);
+
+    try {
+      await AuthQuery.addRefreshToken(id, refreshToken);
+      console.log("webhook add refresh");
+    } catch (e) {
+      try {
+        await AuthQuery.updateRefreshTokenLogin(id, refreshToken);
+        console.log("webhook update refresh");
+      } catch (e) {}
+    }
+
+    return res.redirect(
+      `${AppConfig.Server.frontendBaseUrl}/googleoauthsuccess?accessToken=${accessToken}?refreshToken=${refreshToken}`
+    );
+  }
+);
 
 export default WebhookRouter;
