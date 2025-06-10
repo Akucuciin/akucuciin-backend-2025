cee4ee5d556b7f9c295d6cdb233b49478d91e74b
AghnatHs <aghnathasya@gmail.com>
2025-06-10 21:42:08 +0700

    Security: Verify the Signature to ensure the Notification Authenticity coming from Payment Gateway

--- src/routes/webhook.route.mjs
+++ src/routes/webhook.route.mjs
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import crypto from "node:crypto";
 import passport from "../auth/passport.auth.mjs";
 import AppConfig from "../configs/app.config.mjs";
 import AuthQuery from "../database/queries/auth.query.mjs";
 import OrderQuery from "../database/queries/order.query.mjs";
+import { AuthenticationError } from "../errors/customErrors.mjs";
 import TokenService from "../services/token.service.mjs";
 import { sendOrderPaymentCompletedToCustomer } from "../services/whatsapp.service.mjs";
 import { formatOrderFromDb } from "../utils/order.utils.mjs";
@@ -10,6 +12,29 @@ import { formatOrderFromDb } from "../utils/order.utils.mjs";
 const WebhookRouter = Router();
 
 WebhookRouter.post("/api/payment/webhook", async (req, res) => {
+  // --- Signature Verification
+  const secretKey = AppConfig.PAYMENT.DOKU.secretKey;
+  const receivedSignature = req.headers["x-signature"];
+
+  if (!receivedSignature) {
+    throw new AuthenticationError("Signature not found");
+  }
+
+  const generatedSignature = crypto
+    .createHmac("sha256", secretKey)
+    .update(req.rawBody)
+    .digest("hex");
+
+  const isSignatureValid = crypto.timingSafeEqual(
+    Buffer.from(generatedSignature, "hex"),
+    Buffer.from(receivedSignature, "hex")
+  );
+
+  if (!isSignatureValid) {
+    throw new AuthenticationError("Invalid signature");
+  }
+  // --- End
+
   const notification = req.body;
   const rawInvoice = notification.order.invoice_number;
   const [name, orderId, unique] = rawInvoice.split("::"); // name::orderId::unique
