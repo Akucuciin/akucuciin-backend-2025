5d3fb9af8086d99a083c5c3a33282274439ef98a
AghnatHs <aghnathasya@gmail.com>
2025-06-23 10:37:08 +0700

    Feat: refactor OrderService.create to use validateCouponCodeForOrder from coupon.static-service

--- src/services/order.service.mjs
+++ src/services/order.service.mjs
@@ -1,4 +1,3 @@
-import CouponQuery from "../database/queries/coupon.query.mjs";
 import CustomerQuery from "../database/queries/customer.query.mjs";
 import LaundryPartnerQuery from "../database/queries/laundryPartner.query.mjs";
 import OrderQuery from "../database/queries/order.query.mjs";
@@ -11,6 +10,7 @@ import { formatOrdersFromDb } from "../utils/order.utils.mjs";
 import { generateNanoidWithPrefix } from "../utils/utils.mjs";
 import OrderSchema from "../validators/order.schema.mjs";
 import validate from "../validators/validator.mjs";
+import CouponStaticService from "./coupon.static-service.mjs";
 import {
   sendOrderConfirmationToCustomer,
   sendOrderConfirmationToLaundry,
@@ -57,55 +57,11 @@ const OrderService = {
 
       // COUPON APPLIED
       if (order.coupon_code) {
-        const coupon = await CouponQuery.get(order.coupon_code, trx);
-        if (!coupon) throw new BadRequestError("Kupon tidak ditemukan");
-        if (!coupon.is_active) {
-          throw new BadRequestError(
-            `Gagal, Kupon ${order.coupon_code} sudah tidak aktif`
-          );
-        }
-
-        if (coupon.expired_at) {
-          const isCouponNotExpired = new Date(coupon.expired_at) > new Date();
-
-          if (!isCouponNotExpired) {
-            throw new BadRequestError(
-              `Gagal, Kupon ${order.coupon_code} sudah kadaluarsa dan tidak bisa digunakan`
-            );
-          }
-        }
-
-        if (coupon.customer_id) {
-          // then its only valid for THAT customer only
-          if (coupon.customer_id !== req.user.id) {
-            throw new BadRequestError(
-              `Gagal, Kupon ini tidak valid untuk akun Anda.`
-            );
-          }
-        }
-
-        const allowedPackages = await CouponQuery.getPackages(
-          order.coupon_code,
+        await CouponStaticService.validateCouponCodeForOrder(
+          order,
+          req.user.id,
           trx
         );
-        if (allowedPackages.length > 0) {
-          const allowedIds = allowedPackages.map((p) => p.package_id);
-          if (!allowedIds.includes(order.package_id)) {
-            throw new BadRequestError(
-              `Gagal, Kupon ini hanya berlaku untuk paket tertentu dan tidak berlaku untuk paket yang Anda pilih.`
-            );
-          }
-        }
-
-        if (coupon.is_used === 1) {
-          throw new BadRequestError(
-            `Gagal, Kupon ${order.coupon_code} (sekali pakai), sudah digunakan`
-          );
-        } else if (coupon.is_used === -1) {
-          // Coupon is infinitely used
-        } else if (coupon.is_used === 0) {
-          await CouponQuery.setUsed(order.coupon_code, trx);
-        }
       }
 
       const laundry = await LaundryPartnerQuery.getById(
