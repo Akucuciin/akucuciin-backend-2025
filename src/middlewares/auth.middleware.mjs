499e9bddeebc3edb494f79b208a39d31b142153b
AghnatHs <aghnathasya@gmail.com>
2025-06-23 09:21:26 +0700

    Refactor(auth.middleware)

--- src/middlewares/auth.middleware.mjs
+++ src/middlewares/auth.middleware.mjs
@@ -7,12 +7,19 @@ const authorize = (strategy) => {
       if (strategy == "customer-jwt") message = "Unauthorized, only customer";
       else if (strategy == "admin-jwt") message = "Unauthorized, only admin";
       else if (strategy == "driver-jwt") message = "Unauthorized, only driver";
-      else if (strategy == "laundry-partner-jwt") message = "Unauthorized, only partner";
+      else if (strategy == "laundry-partner-jwt")
+        message = "Unauthorized, only partner";
       if (err) {
         return next(err);
       }
       if (!user) {
-        return res.status(401).json({ success: false, message: message, error: info?.message || null });
+        return res
+          .status(401)
+          .json({
+            success: false,
+            message: message,
+            error: info?.message || null,
+          });
       }
       req.user = user;
       next();
