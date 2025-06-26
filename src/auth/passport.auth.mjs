907408f9f4eb682a23352eb9520b981b68762a73
AghnatHs <aghnathasya@gmail.com>
2025-06-27 01:19:27 +0700

    perf: Remove database query from jwt token checking

--- src/auth/passport.auth.mjs
+++ src/auth/passport.auth.mjs
@@ -2,10 +2,7 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { ExtractJwt, Strategy as JwtStrategy } from "passport-jwt";
 import AppConfig from "../configs/app.config.mjs";
-import AdminQuery from "../database/queries/admin.query.mjs";
 import CustomerQuery from "../database/queries/customer.query.mjs";
-import DriverQuery from "../database/queries/driver.query.mjs";
-import LaundryPartnerQuery from "../database/queries/laundryPartner.query.mjs";
 import { generateNanoidWithPrefix } from "../utils/utils.mjs";
 
 var opts = {
@@ -79,7 +76,7 @@ passport.use(
       return done(null, false);
     }
     var user = jwtPayload;
-    const isValidCustomer = await CustomerQuery.isValidCustomer(user.id);
+    const isValidCustomer = user.role === "customer";
     if (isValidCustomer) return done(null, user);
     else return done(null, false);
   })
@@ -93,7 +90,7 @@ passport.use(
       return done(null, false);
     }
     var user = jwtPayload;
-    const isValidAdmin = await AdminQuery.isValidAdmin(user.id);
+    const isValidAdmin = user.role === "admin";
     if (isValidAdmin) return done(null, user);
     else return done(null, false);
   })
@@ -107,7 +104,7 @@ passport.use(
       return done(null, false);
     }
     var user = jwtPayload;
-    const isValidDriver = await DriverQuery.isValidDriver(user.id);
+    const isValidDriver = user.role === "driver";
     if (isValidDriver) return done(null, user);
     else return done(null, false);
   })
@@ -121,7 +118,7 @@ passport.use(
       return done(null, false);
     }
     var user = jwtPayload;
-    const isValidPartner = await LaundryPartnerQuery.isValidPartner(user.id);
+    const isValidPartner = user.role === "laundry-partner";
     if (isValidPartner) return done(null, user);
     else return done(null, false);
   })
