f27beceb15b580e50b11bf4d6f58fc489230108d
AghnatHs <aghnathasya@gmail.com>
2025-07-18 20:01:43 +0700

    Versioning stable[main]

--- src/routes/version.route.mjs
+++ src/routes/version.route.mjs
@@ -6,7 +6,7 @@ const VersionRouter = Router();
 VersionRouter.get('/api/version', useRateLimiter(5, 1), (req, res) => {
   res.json({
     version: '16.07.2025.23-09',
-    status: 'stable[main-bump-packages]',
+    status: 'stable[main]',
   });
 });
 
